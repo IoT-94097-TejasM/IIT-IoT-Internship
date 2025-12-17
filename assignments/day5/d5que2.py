a651681f4e58af0826da755503112708ca629463
tejasmali31 <tejasmali315@gmail.com>
2025-12-17 12:45:51 +0530

    d5que

---
+++ assignments/day5/d5que2.py
@@ -0,0 +1,8 @@
+import calcu
+num1=int(input("enter the number:"))
+num2=int(input("enter the numebr:"))
+
+print("addition",calcu.add(num1,num2))
+print("substraction",calcu.sub(num1,num2))
+print("multiplication",calcu.mul(num1,num2))
+print("division",calcu.div(num1,num2))
